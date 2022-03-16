72facdb809756c94ec272cdbb073a5051cc8c05b
Sheng Lv 吕晟 <sheng.lyu@rakwireless.com>
2022-03-16 08:44:37 +0000

    initialize the sensor only once

--- sensors/rak16000/rak16000-power-monitoring.py
+++ sensors/rak16000/rak16000-power-monitoring.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+"""
+Reads values from a analog input using the RAK16001 module
+"""
+__copyright__ = "Copyright 2022, RAKwireless"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__status__ = "Production"
+__maintainer__ = "rakwireless.com"
+
 from ina219 import INA219
 from ina219 import DeviceRangeError
 import time
@@ -6,16 +15,17 @@ import time
 # RAK16000 has a 100 mΩ (0.1 Ω) shunt resistor placed between the power supply and the load
 SHUNT_OHMS = 0.1
 
+# The default 7-bit I2C address is 0x41
+ina = INA219(SHUNT_OHMS, address=0x41)
+ina.configure()
+
 
 def read():
-    # The default 7-bit I2C address is 0x41
-    ina = INA219(SHUNT_OHMS, address=0x41)
-    ina.configure()
-
-    print("Bus Voltage: %.3f V" % ina.voltage())
+    # print("Bus Voltage: %.3f V" % ina.voltage())
     try:
+        print("Bus Voltage: %.3f V" % ina.voltage())
         print("Bus Current: %.3f mA" % ina.current())
-        print("Power: %.3f W" % (ina.power()/1000))
+        print("Power: %.3f mW" % (ina.power() / 1000))
         print("Shunt voltage: %.3f mV" % ina.shunt_voltage())
         print()
     except DeviceRangeError as e:
