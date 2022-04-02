6467ed2a64ce9467edb8e9c67b2d399252b520d0
XiKai <kai.xi@rakwireless.com>
2022-04-02 03:48:42 +0100

    modify rak12004 example

--- sensors/rak12004/rak12004-reading/rak12004-reading.py
+++ sensors/rak12004/rak12004-reading/rak12004-reading.py
@@ -9,7 +9,7 @@ __status__ = "Production"
 
 
 
-from mq2 import mq2
+from mqx import mqx
 import time
 import RPi.GPIO as GPIO
 
@@ -25,21 +25,21 @@ GPIO.setup(EN_PIN, GPIO.OUT)
 GPIO.output(EN_PIN, GPIO.HIGH)
 time.sleep(0.5)
 
-sensor = mq2.MQ2()
+mq2 = mqx.MQx()
 
-sensor.config_cycle_time(mq2.CYCLE_TIME_32)
+mq2.config_cycle_time(mq2.CYCLE_TIME_32)
 #in this example, we test ppm of smoke.
-sensor.set_slope(mq2.SMOKE_SLOPE)
-sensor.set_intercept_y(mq2.SMOKE_INTERCEPT_Y)
+mq2.set_slope(mq2.MQ2_SMOKE_SLOPE)
+mq2.set_intercept_y(mq2.MQ2_SMOKE_INTERCEPT_Y)
 
 # we have calibrated Ro value in our envirenment.
-sensor.set_Ro(mq2.RO)
+mq2.set_Ro(mq2.MQ2_RO)
 
 #you also can recalibrate Ro with method: calibrate_Ro
-#sensor.calibrate_Ro()
+#sensor.calibrate_Ro(mq2.MQ2_RATIO_AIR)
 try:
     while True:
-        ppm = sensor.calibrate_ppm()
+        ppm = mq2.calibrate_ppm()
         print("ppm: {:.2f}".format(ppm))
         time.sleep(5)
 
