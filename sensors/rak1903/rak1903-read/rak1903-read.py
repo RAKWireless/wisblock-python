a99e6ea89f6f1950cc980d3d7d02996ccd4f88c2
Sheng Lv 吕晟 <sheng.lyu@rakwireless.com>
2022-05-09 08:21:02 +0000

    Update rak1903-read.py

--- sensors/rak1903/rak1903-read/rak1903-read.py
+++ sensors/rak1903/rak1903-read/rak1903-read.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 """
 Read ambient light data with RAK1903(OPT3001).
 """
@@ -9,14 +9,15 @@ __status__ = "Production"
 __maintainer__ = "rakwireless.com"
 
 import time
-from opt3001 import opt3001 
+from opt3001 import opt3001
+# opt3001 has 4 possible I2C address: 0x44, 0x45, 0x46, and 0x47. It is set to ox44 on RAK1903
 address = 0x44
 
-opt = opt3001.OPT3001(address) 
+opt = opt3001.OPT3001(address)
 
 # Configure to run in Continuous conversions mode
 opt.write_config_reg(opt3001.I2C_LS_CONFIG_CONT_FULL_800MS)
 
-while(True):
-  print("Ambient light:", opt.read_lux_float(), "lux")
-  time.sleep(1)
+while True:
+    print("Ambient light: %.2f lux" % opt.read_lux_float())
+    time.sleep(1)
