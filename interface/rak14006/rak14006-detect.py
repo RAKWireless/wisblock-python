7414c6d0f3809f9a7d0f2e51f97275802213576c
Sheng Lv 吕晟 <sheng.lyu@rakwireless.com>
2022-05-09 09:29:51 +0000

    Update rak14006-detect.py

--- interface/rak14006/rak14006-detect.py
+++ interface/rak14006/rak14006-detect.py
@@ -13,16 +13,17 @@ from datetime import timedelta
 
 '''
 if you mount rak14006 on the PI-Hat IO slot 1, the Counter Clockwise(CCW) pin is 16 
-and Clockwise(CW) pin is 12, Button Switch(SW) pin is 6. if you use PI-HAT IO slot 2, 
-change CCW_PIN to 24,  CW_PIN to 22, SW_PIN to 21.
+and Clockwise(CW) pin is 12, Button Switch(SW) pin is 6. 
+If you use PI-HAT IO slot 2, change CCW_PIN to 24,  CW_PIN to 22, SW_PIN to 21.
 '''
 CCW_PIN = 16
 CW_PIN = 12
 SW_PIN = 6
-#position is 0 when start up
+# position is 0 when start up
 position = 0
-
+# gpiochip0 [pinctrl-bcm2711] (58 lines)
 chip = gpiod.chip(0)
+
 ccw_line = chip.get_line(CCW_PIN)
 cw_line = chip.get_line(CW_PIN)
 sw_line = chip.get_line(SW_PIN)
@@ -41,18 +42,17 @@ try:
             event = ccw_line.event_read()
             if event.event_type == gpiod.line_event.RISING_EDGE:
                 position -= 1
-                print("position step is %d"%position)
+                print("position step is %d" % position)
         if cw_line.event_wait(timedelta(seconds=0.1)):
             event = cw_line.event_read()
             if event.event_type == gpiod.line_event.RISING_EDGE:
                 position += 1
-                print("position step is %d"%position)
+                print("position step is %d" % position)
         if sw_line.event_wait(timedelta(seconds=0.1)):
             event = sw_line.event_read()
             if event.event_type == gpiod.line_event.FALLING_EDGE:
                 print("button press")
-        
+
 except KeyboardInterrupt:
     print("ctrl+c...")
     exit()
-
