9b95648619a63951e0c0fcd8ca6d40ae85bc3bb5
XiKai <kai.xi@rakwireless.com>
2022-05-26 10:30:07 +0100

    update rak13004 script with libgpiod

--- interface/rak13004/rak13004_breath.py
+++ interface/rak13004/rak13004_breath.py
@@ -7,63 +7,21 @@ __license__ = "GPL"
 __version__ = "1.0.0"
 __status__ = "Production"
 
-import time
-from smbus2 import SMBus
+
+import gpiod
 import Adafruit_PCA9685	
-import json
 
-PATH = '/etc/gpio_expander.json'
 
-Port = ["IO0_0", "IO0_1", "IO0_2", "IO0_3", "IO0_4", "IO0_5", "IO0_6", "IO0_7",
-            "IO1_0", "IO1_1", "IO1_2", "IO1_3", "IO1_4", "IO1_5", "IO1_6", "IO1_7",
-            "IO2_0", "IO2_1", "IO2_2", "IO2_3", "IO2_4", "IO2_5", "IO2_6", "IO2_7"
-]
-
-def getPortNum(name):
-    for i in range(0, 24):
-        if Port[i] == name:
-            return i
-def write_value(name, value):
-
-    if name not in Port:
-        print("Invalid IO port name!")
-        sys.exit()
-    if value != 0 and value != 1:
-        print("Invalid value to write!")
-        sys.exit()
-
-    save = {}
-    with open(PATH,'rb') as f:
-        cur = json.load(f)
-        cur["status"][name] = value
-        save = cur
-    f.close()
-
-    with open(PATH,'w') as r:
-        json.dump(save,r)
-    r.close()
-
-    value = [0xff, 0xff, 0xff]
-
-    for tmp in Port:
-
-        if save["status"][tmp] == 0:
-            num = getPortNum(tmp)
-            if num in range(0, 8):
-                value[2] &= ~(1 << num)
-            if num in range(8, 16):
-                value[1] &= ~(1 << (num - 8))
-            if num in range(16, 24):
-                value[0] &= ~(1 << (num - 16))
-    #print(value)
-
-    with SMBus(1) as bus:
-        bus.write_i2c_block_data(0x20, 0, value)
+CHIP_NUM = 2
+OE_PIN = 6
 
 #active LOW output enable for PCA9685 OE PIN
-write_value("IO1_6", 0)
-write_value("IO1_7", 0)
-
+chip = gpiod.chip(CHIP_NUM)
+line = chip.get_line(OE_PIN)
+config = gpiod.line_request()
+config.request_type = gpiod.line_request.DIRECTION_OUTPUT
+line.request(config)
+line.set_value(0)
 
 
 # Initialise the PCA9685 using the default address (0x40).
@@ -71,7 +29,6 @@ pwm = Adafruit_PCA9685.PCA9685()
 
 # Alternatively specify a different address and/or bus:
 #pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
-
 # Set frequency to 60hz
 pwm.set_pwm_freq(60)
 
@@ -82,9 +39,8 @@ pwm.set_pwm_freq(60)
 #    Long leg goes to PWM pin on channel 0
 
 while True:
-	for i in range(0, 4096, 10):
+	for i in range(0, 4096, 5):
 		pwm.set_pwm(0, 0, i)
 
-	for i in range(0, 4096, 10):
+	for i in range(0, 4096, 5):
 		pwm.set_pwm(0, 0, 4095-i)
-
