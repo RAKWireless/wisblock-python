98d55ef776327479e31b450b6f2e0310b6ba1939
XiKai <kai.xi@rakwireless.com>
2022-05-26 10:31:52 +0100

    update rak13004 script with libgpiod

--- interface/rak5811/rak5811_read.py
+++ interface/rak5811/rak5811_read.py
@@ -9,71 +9,26 @@ __status__ = "Production"
 __maintainer__ = "rakwireless.com"
 
 import time
-import sys
-import logging
 import Adafruit_ADS1x15
-from smbus2 import SMBus
-import json
+import gpiod
 
 
-GAIN 			= 1
-#only analog input 1 can work now.
-ANALOG_INPUT_1 	= 1
-SAMPLE_NUM		= 32
+GAIN = 1
+# WisBlock Slot#1: AIN0 = 0, AIN1 = 1
+# WisBlock Slot#2: AIN0 = 2, AIN1 = 3
+AIN0 = 0
+AIN1 = 1
+SAMPLE_NUM = 32
+ENABLE_PIN = 14
 
-PATH = '/etc/gpio_expander.json'
+# set rak5811 enable pin to high
+chip = gpiod.chip(2)
+line = chip.get_line(ENABLE_PIN)
+config = gpiod.line_request()
+config.request_type = gpiod.line_request.DIRECTION_OUTPUT
+line.request(config)
+line.set_value(1)
 
-Port = ["IO0_0", "IO0_1", "IO0_2", "IO0_3", "IO0_4", "IO0_5", "IO0_6", "IO0_7",
-            "IO1_0", "IO1_1", "IO1_2", "IO1_3", "IO1_4", "IO1_5", "IO1_6", "IO1_7",
-            "IO2_0", "IO2_1", "IO2_2", "IO2_3", "IO2_4", "IO2_5", "IO2_6", "IO2_7"
-]
-
-def getPortNum(name):
-    for i in range(0, 24):
-        if Port[i] == name:
-            return i
-
-def write_value(name, value):
-
-    if name not in Port:
-        print("Invalid IO port name!")
-        sys.exit()
-    if value != 0 and value != 1:
-        print("Invalid value to write!")
-        sys.exit()
-
-    save = {}
-    with open(PATH,'rb') as f:
-        cur = json.load(f)
-        cur["status"][name] = value
-        save = cur
-    f.close()
-
-    with open(PATH,'w') as r:
-        json.dump(save,r)
-    r.close()
-
-    value = [0xff, 0xff, 0xff]
-
-    for tmp in Port:
-
-        if save["status"][tmp] == 0:
-            num = getPortNum(tmp)
-            if num in range(0, 8):
-                value[2] &= ~(1 << num)
-            if num in range(8, 16):
-                value[1] &= ~(1 << (num - 8))
-            if num in range(16, 24):
-                value[0] &= ~(1 << (num - 16))
-    #print(value)
-
-    with SMBus(1) as bus:
-        bus.write_i2c_block_data(0x20, 0, value)
-
-
-#set enable PIN to HIGH
-write_value("IO1_4", 1)
-write_value("IO1_5", 1)
 
 """
 reading rak5811 analog input via ads115 module 
@@ -84,7 +39,7 @@ try:
 		total = 0
 		for i in range(0,SAMPLE_NUM):
 			# read rak5801 analog input 0
-			value = adc.read_adc(ANALOG_INPUT_1, gain=GAIN)
+			value = adc.read_adc(AIN0, gain=GAIN)
 			total += value
 		raw = total / SAMPLE_NUM
 		"""
@@ -98,5 +53,5 @@ try:
 		print("%.2f v"%raw_voltage)
 
 except KeyboardInterrupt:
-    logging.info('ctrl + c:')
+    print('ctrl + c:')
     exit()
